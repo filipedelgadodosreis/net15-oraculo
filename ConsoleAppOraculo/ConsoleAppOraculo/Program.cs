f8134d7cbe08642f8edce76e4d87cda42a722e8c
filipedelgadodosreis <filipedelgadodosreis@gmail.com>
2019-08-14 10:51:52 -0300

    Initial
    Commit

---
+++ ConsoleAppOraculo/ConsoleAppOraculo/Program.cs
@@ -0,0 +1,41 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ConsoleAppOraculo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string oraculo = string.Empty;
+
+            var connection = ConnectionMultiplexer.Connect("40.77.24.62");
+            var db = connection.GetDatabase();
+
+            try
+            {
+                ISubscriber sub = connection.GetSubscriber();
+
+                sub.Subscribe("Perguntas", (ch, msg) =>
+                {
+                    Console.WriteLine(msg.ToString());
+
+                    var chave = msg.ToString().Split(":");
+                    oraculo = msg;
+
+                    db.HashSet(chave[0].ToString(), "Equipe: Fundao", "Brasilia");
+                });
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Problemas de comunicação {ex.Message}");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
+    
+
